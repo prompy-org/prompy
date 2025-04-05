c6f2602781c7492ccabdaf7ca0de178f4ede3247
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-04-06 05:06:52 +0530

    add proper url

--- public/background.js
+++ public/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(() => {
   
   // Set default options
   chrome.storage.sync.set({
-    apiUrl: process.env.VITE_API_URL,
+    apiUrl: 'http://localhost:5000/api',
     theme: 'system',
     syncFrequency: 15
   }, () => {

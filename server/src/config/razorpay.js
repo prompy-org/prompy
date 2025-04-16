b3f30145d7df66d0bbf2f223a1f7cd034434a944
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-04-16 08:32:53 +0530

    razorpay key id

--- server/src/config/razorpay.js
+++ server/src/config/razorpay.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Initialize Razorpay instance with environment variables
+const RAZORPAY_KEY_ID = process.env.NODE_ENV === 'PROD' ? process.env.PROD_RAZORPAY_KEY_ID : process.env.DEV_RAZORPAY_KEY_ID;
+const RAZORPAY_KEY_SECRET = process.env.NODE_ENV === 'PROD' ? process.env.PROD_RAZORPAY_KEY_SECRET : process.env.DEV_RAZORPAY_KEY_SECRET;
+
 const razorpayInstance = new Razorpay({
-  key_id: process.env.NODE_ENV === 'PROD' ? process.env.PROD_RAZORPAY_KEY_ID : process.env.DEV_RAZORPAY_KEY_ID,
-  key_secret: process.env.NODE_ENV === 'PROD' ? process.env.PROD_RAZORPAY_KEY_SECRET : process.env.DEV_RAZORPAY_KEY_SECRET
+  key_id: RAZORPAY_KEY_ID,
+  key_secret: RAZORPAY_KEY_SECRET
 });
 
 export default razorpayInstance;
